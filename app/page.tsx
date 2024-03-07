c6b0b6313a4add2c9e2e65b6b9009174bda69dea
marsonmao <marsonmao@gmail.com>
2024-03-07 17:40:13 +0800

    aaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,37 @@
 "use client";
 
-import { useEffect } from "react";
-import { MineSweeperCore } from "./_manager";
+import { useCallback, useEffect, useState } from "react";
+import { MineSweeperClassicMode } from "./_manager";
 
-const msm = new MineSweeperCore({
-  rowSize: 4,
-  columnSize: 4,
-  maxLifeCount: 1,
-});
-
-msm.deployMinesFromCell({ row: 0, column: 0 }, 2);
-msm.openCell({ row: 0, column: 0 });
+// const msm = new MineSweeperClassicMode({});
+// msm.deployMinesFromCell({ row: 0, column: 0 }, 2);
+// msm.openCell({ row: 0, column: 0 });
 
 export default function Home() {
-  console.log(msm.getCellMineDatas());
-  console.log(msm.getCellStates());
-  return <div>1</div>;
+  const onGameEnd = useCallback(() => {
+    console.log(1);
+  }, []);
+  const [game] = useState(
+    () =>
+      new MineSweeperClassicMode({
+        onGameEnd,
+      })
+  );
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          game.startGame({
+            cell: { row: 0, column: 0 },
+            size: { row: 8, column: 8 },
+            mineCount: 12,
+          })
+        }
+      >
+        start
+      </button>
+      <div>123</div>
+    </div>
+  );
 }
