6a331c0363c052ab118a221b1bcba2effcf01d0a
marsonmao <marsonmao@gmail.com>
2024-03-08 21:59:38 +0800

    feat: simplify functions

--- app/classic/page.tsx
+++ app/classic/page.tsx
@@ -8,60 +8,46 @@ import {
 } from "@/app/_components";
 import { Cell, MineSweeperClassicMode } from "@/app/_game";
 import { config, disableContextMenu } from "@/app/_react_game";
-import { ComponentProps, useCallback, useState } from "react";
+import { ComponentProps, useCallback, useRef, useState } from "react";
 
 const difficulties = config.classic.boards;
 
 export default function Home() {
-  const [notification, setNotification] = useState("");
   const [_, rerender] = useState(0);
+  const [notification, setNotification] = useState("");
   const [isEnded, setIsEnded] = useState(false);
   const [difficultyIndex, setDifficultyIndex] = useState(0);
   const currentDifficulty = difficulties[difficultyIndex];
 
-  const onGameLost = useCallback(() => {
-    setNotification("Good game -- restart?");
-    setIsEnded(true);
-  }, []);
-
-  const onGameWon = useCallback(() => {
-    setNotification("You have won! -- restart?");
-    setIsEnded(true);
-  }, []);
-
-  const [game] = useState(() => {
-    const game = new MineSweeperClassicMode({
-      onGameWon,
-      onGameLost,
-    });
-    game.initGame({
-      ...currentDifficulty,
-      size: { ...currentDifficulty },
-    });
-    return game;
-  });
-
-  const initGame = useCallback(
-    ({ row, column, mineCount }: (typeof config.classic.boards)[number]) => {
-      game.initGame({
-        size: { row, column },
-        mineCount,
+  const game = useRef(
+    (function create() {
+      const game = new MineSweeperClassicMode({
+        onGameWon: () => {
+          setNotification("You have won! -- restart?");
+          setIsEnded(true);
+        },
+        onGameLost: () => {
+          setNotification("Good game -- restart?");
+          setIsEnded(true);
+        },
       });
-      setNotification("");
-      setIsEnded(false);
-    },
-    [game],
+      game.initGame({
+        ...currentDifficulty,
+        size: { ...currentDifficulty },
+      });
+      return game;
+    })(),
   );
 
   const getCellProps = useCallback(
     ({ row, column }: Cell) => {
       const cellProps: ComponentProps<typeof CellComponent> = {
-        state: game.getCellStates()[row][column],
-        data: game.getCellMineDatas()[row][column],
+        state: game.current.getCellStates()[row][column],
+        data: game.current.getCellMineDatas()[row][column],
         className: isEnded ? "pointer-events-none" : undefined,
         onClick: (e) => {
           const { row, column } = e.currentTarget.dataset;
-          game.clickCell({
+          game.current.clickCell({
             row: Number(row),
             column: Number(column),
           });
@@ -69,7 +55,7 @@ export default function Home() {
         },
         onContextMenu: (e) => {
           const { row, column } = e.currentTarget.dataset;
-          game.flagCell({
+          game.current.flagCell({
             row: Number(row),
             column: Number(column),
           });
@@ -77,7 +63,7 @@ export default function Home() {
         },
         onDoubleClick: (e) => {
           const { row, column } = e.currentTarget.dataset;
-          game.clearAdjacentCells({
+          game.current.clearAdjacentCells({
             row: Number(row),
             column: Number(column),
           });
@@ -86,21 +72,26 @@ export default function Home() {
       };
       return cellProps;
     },
-    [game, isEnded],
+    [isEnded],
   );
 
   const handleDifficultySelect = useCallback<
     ComponentProps<typeof DifficultySelector>["onDifficultySelect"]
-  >(
-    (nextIndex) => {
-      initGame({ ...difficulties[nextIndex] });
-      setDifficultyIndex(nextIndex);
-    },
-    [initGame],
-  );
+  >((nextIndex) => {
+    const nextDifficulty = difficulties[nextIndex];
 
-  const boardRowSize = game.getCellMineDatas().length;
-  const boardColumnSize = game.getCellMineDatas()[0].length;
+    game.current.initGame({
+      size: { row: nextDifficulty.row, column: nextDifficulty.column },
+      mineCount: nextDifficulty.mineCount,
+    });
+
+    setNotification("");
+    setIsEnded(false);
+    setDifficultyIndex(nextIndex);
+  }, []);
+
+  const boardRowSize = game.current.getCellMineDatas().length;
+  const boardColumnSize = game.current.getCellMineDatas()[0].length;
   const headlineText = notification || "...";
 
   return (
